d13400c1e391db1c5921784e22d9524dc4a6512f
AzizBenIsmail <aziz.270700@gmail.com>
2026-04-03 18:21:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ require("dotenv").config();
 async function helloHedera() {
   //Grab your Hedera testnet account ID and private key from your .env file
   const myAccountId = process.env.MY_ACCOUNT_ID;
-  const myPrivateKey = process.env.MY_PRIVATE_KEY;
+  const myPrivateKeyString = process.env.MY_PRIVATE_KEY;
 
   // If we weren't able to grab it, we should throw a new error
-  if (!myAccountId || !myPrivateKey) {
+  if (!myAccountId || !myPrivateKeyString) {
     throw new Error(
       "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present"
     );
@@ -22,6 +22,9 @@ async function helloHedera() {
 
   //Create your Hedera Testnet client
   const client = Client.forTestnet();
+
+  //Decode the private key from HEX string
+  const myPrivateKey = PrivateKey.fromStringED25519(myPrivateKeyString);
 
   //Set your account as the client's operator
   client.setOperator(myAccountId, myPrivateKey);
