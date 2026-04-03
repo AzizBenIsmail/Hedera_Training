e3dbd66db81d48fa06d3e535a994f17b95a79296
AzizBenIsmail <aziz.270700@gmail.com>
2026-04-03 18:18:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,39 +43,39 @@ async function helloHedera() {
   //Log the account ID
   console.log("The new account ID is: " + newAccountId);
 
-  // //Verify the account balance
-  // const accountBalance = await new AccountBalanceQuery()
-  //   .setAccountId(newAccountId)
-  //   .execute(client);
+  //Verify the account balance
+  const accountBalance = await new AccountBalanceQuery()
+    .setAccountId(newAccountId)
+    .execute(client);
 
-  // console.log(
-  //   "The new account balance is: " +
-  //     accountBalance.hbars.toTinybars() +
-  //     " tinybar."
-  // );
+  console.log(
+    "The new account balance is: " +
+      accountBalance.hbars.toTinybars() +
+      " tinybar."
+  );
 
-  // //Create the transfer transaction
-  // const sendHbar = await new TransferTransaction()
-  //   .addHbarTransfer(myAccountId, Hbar.fromTinybars(-10000)) //Sending account
-  //   .addHbarTransfer(newAccountId, Hbar.fromTinybars(10000)) //Receiving account
-  //   .execute(client);
+  //Create the transfer transaction
+  const sendHbar = await new TransferTransaction()
+    .addHbarTransfer(myAccountId, Hbar.fromTinybars(-10000)) //Sending account
+    .addHbarTransfer(newAccountId, Hbar.fromTinybars(10000)) //Receiving account
+    .execute(client);
 
-  // //Verify the transaction reached consensus
-  // const transactionReceipt = await sendHbar.getReceipt(client);
-  // console.log(
-  //   "The transfer transaction from my account to the new account was: " +
-  //     transactionReceipt.status.toString()
-  // );
+  //Verify the transaction reached consensus
+  const transactionReceipt = await sendHbar.getReceipt(client);
+  console.log(
+    "The transfer transaction from my account to the new account was: " +
+      transactionReceipt.status.toString()
+  );
 
-  // //Verify the account balance
-  // const newAccountBalance = await new AccountBalanceQuery()
-  //   .setAccountId(newAccountId)
-  //   .execute(client);
+  //Verify the account balance
+  const newAccountBalance = await new AccountBalanceQuery()
+    .setAccountId(newAccountId)
+    .execute(client);
 
-  // console.log(
-  //   "The new account balance is: " +
-  //     newAccountBalance.hbars.toTinybars() +
-  //     " tinybar."
-  // );
+  console.log(
+    "The new account balance is: " +
+      newAccountBalance.hbars.toTinybars() +
+      " tinybar."
+  );
 }
 helloHedera();
